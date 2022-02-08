3fe6562c3213c5e4f4aa5e189a0e5914955e1cdd
HuPeng333 <2237803016@qq.com>
2022-02-08 13:54:05 +0800

    feat: 将重新登录改为可选的

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -18,15 +18,71 @@ function startObserver () {
 
   const observer = new MutationObserver(() => {
     const mask = document.getElementById(MASK_ID)
-    console.log(mask)
     if (mask) {
       // 删除蒙层
       mask.remove()
-      window.open(RE_LOGIN_URL)
+      createToast('登录已过期，是否需要重新登录?', () => {
+        window.open(RE_LOGIN_URL)
+        createToast('正在重新登录，请等待新打开的窗口加载完毕！')
+      }, () => {
+        window.close()
+      })
     }
   })
 
   observer.observe(document.body, {
     childList: true
   })
+}
+
+
+/**
+ * 创建对话框
+ * @param msg {string} 对话框信息
+ * @param confirmCallback {function} 确认后的回调
+ * @param cancelCallback {function} 取消后的回调
+ */
+function createToast(msg, confirmCallback = undefined, cancelCallback = undefined) {
+  const TOAST_ID = "wtu-relogin-plugin-toast"
+  const CONFIRM_BUTTON_ID = "wtu-relogin-plugin-btn-confirm"
+  const CANCEL_BUTTON_ID = "wtu-relogin-plugin-btn-cancel"
+
+  const TOAST_HTML = `
+  <div class="wtu-relogin-plugin-toast" id="${TOAST_ID}">
+    <div class="wtu-relogin-plugin-toast-block">
+      <div class="wtu-relogin-plugin-toast-title">
+        <span>提示</span>
+      </div>
+      <div class="wtu-relogin-plugin-toast-content">
+        <span>${msg}</span>
+      </div>
+      <div class="wtu-relogin-plugin-toast-btn-area">
+        <button class="wtu-relogin-plugin-btn-confirm" id="${CONFIRM_BUTTON_ID}">确定</button>
+        <button class="wtu-relogin-plugin-btn" id="${CANCEL_BUTTON_ID}">取消</button>
+      </div>
+    </div>
+  </div>
+  `
+  $(document.body).append(TOAST_HTML)
+
+  document.getElementById(CONFIRM_BUTTON_ID).onclick = () => {
+    closeToast()
+    executeIfExist(confirmCallback)
+  }
+  document.getElementById(CANCEL_BUTTON_ID).onclick = () => {
+    closeToast()
+    executeIfExist(cancelCallback)
+  }
+
+  const closeToast = () => {
+    document.getElementById(TOAST_ID).remove()
+  }
+}
+
+/**
+ * 如果存在则执行该函数
+ * @param callback {Function}
+ */
+function executeIfExist(callback = undefined) {
+  callback ? callback() : ''
 }
